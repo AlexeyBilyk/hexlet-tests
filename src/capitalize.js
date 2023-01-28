dda066aa1333cb021ff71eb97622e11b2eece48f
Alexey Bilyk <maverick4730@gmail.com>
2023-01-28 15:03:44 +0300

    create

---
+++ src/capitalize.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const capitalize = (text) => {
+  if (text === '') {
+    return '';
+  }
+  const firstChar = text[0].toUpperCase();
+  const restSubstring = text.slice(1);
+  return `${firstChar}${restSubstring}`;
+};
+
+ export default capitalize; 
